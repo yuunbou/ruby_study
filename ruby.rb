ed35067e4c2a73bd88b8e3b79466b5cdcb412531
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-03-04 13:54:26 +0000

    [Create] Ruby学習用フォルダ作成

---
+++ ruby.rb
@@ -0,0 +1,18 @@
+User = "太郎"
+puts User
+
+rubystudy = "Rubyの学習"
+puts rubystudy
+
+puts 100
+puts 10 + 3  # 足し算
+puts 10 - 3  # 引き算
+puts 10 * 3  # 掛け算
+puts 90 / 3  # 割り算
+puts 100 % 3  # 割り算の余り
+
+name = "太郎"
+weight = 50
+
+# to_sは整数を文字列型へ変換する
+puts name + "さんの体重は" + weight.to_s + "kgです"  #
