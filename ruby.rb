699518fb025f74e4e881407852bd4b15b2e1d279
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-05-19 15:06:55 +0000

    [Study]デフォルト値付きの引数2

--- ruby.rb
+++ ruby.rb
@@ -1,11 +1,20 @@
 # デフォルト値付きの引数
-def greet(country = 'japan')
-  if country == 'japan'
-    puts 'こんにちは'
-  else
-    puts 'Hello'
-  end
+def default_args(a, b, c = 0, d = 0)
+  puts "a=#{a}, b=#{b}, c=#{c}, d=#{d}"
 end
 
-greet
-greet('us')
+default_args(1, 2)
+default_args(1, 2, 3)
+default_args(1, 2, 3, 4)
+
+# システム日時や他のメソッドの戻り値をデフォルト値に指定する
+
+def foo(time = Time.now, message = bar)
+  puts "time: #{time}, message: #{message}"
+end
+
+def bar
+  'BAR'
+end
+
+foo
