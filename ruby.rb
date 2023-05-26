12c398334cf0752d4861813435719f5c41f702f1
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-05-26 14:53:10 +0000

    [Study]参照の概念を理解する2

--- ruby.rb
+++ ruby.rb
@@ -1,19 +1,16 @@
 # aとbはどちらも同じ文字列だが、オブジェクトとしては別物
 a = 'hello'
 b = 'hello'
-puts a.object_id
-puts b.object_id
-
-#cにbを代入する。bとcはどちらも同じオブジェクト
 c = b
-puts c.object_id
 
-# メソッドの引数にcを渡す。引数として受け取ったdはb、cと同じオブジェクトを参照している
-def m(d)
-  d.object_id
+# 渡された文字列を破壊的に大文字に変換するメソッドを定義する
+def m!(d)
+  d.upcase!
 end
-puts m(c)
+puts m!(c)
 
 # ewual?メソッドを使って同じオブジェクトかどうか確認しても良い
-puts a.equal?(b)
-puts b.equal?(c)
+p b
+p c
+
+p a
