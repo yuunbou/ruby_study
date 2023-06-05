fe471536dea03b3e23f8c96563195bfb23045ef0
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-06-05 16:02:58 +0000

    [Study] FizzBuzzテストコード作成

--- ruby.rb
+++ ruby.rb
@@ -1,16 +1,32 @@
-# aとbはどちらも同じ文字列だが、オブジェクトとしては別物
-a = 'hello'
-b = 'hello'
-c = b
-
-# 渡された文字列を破壊的に大文字に変換するメソッドを定義する
-def m!(d)
-  d.upcase!
+def fizz_buzz(n)
+  if n % 15 == 0
+    "FizzBuzz"
+  elsif n % 3 == 0
+    "Fizz"
+  elsif n % 5 == 0
+    "Buzz"
+  else
+    n.to_s
+  end
 end
-puts m!(c)
 
-# ewual?メソッドを使って同じオブジェクトかどうか確認しても良い
-p b
-p c
+require 'minitest/autorun'
 
-p a
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal '4', fizz_buzz(4)
+    assert_equal 'Buzz', fizz_buzz(5)
+    assert_equal 'Fizz', fizz_buzz(6)
+    assert_equal 'FizzBuzz', fizz_buzz(15)
+  end
+end
+
+# puts "数字を入力してください"
+
+# input = gets.to_i
+
+# puts "結果は。。。"
+# puts fizz_buzz(input)
